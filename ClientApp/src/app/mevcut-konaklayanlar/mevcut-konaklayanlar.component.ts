1e7b2ba44aa297155b94bc148a2d2da5bf78fa73
mucahitgoktas <mucahitgoktas@gmail.com>
2021-07-04 00:24:23 +0300

    Cumartesi Gecesi Bitti

---
+++ ClientApp/src/app/mevcut-konaklayanlar/mevcut-konaklayanlar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mevcut-konaklayanlar',
+  templateUrl: './mevcut-konaklayanlar.component.html',
+  styleUrls: ['./mevcut-konaklayanlar.component.css']
+})
+export class MevcutKonaklayanlarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
