0205fa8159665eab1f13198136d5e9d75697fd1c
mucahitgoktas <mucahitgoktas@gmail.com>
2021-07-27 16:27:50 +0300

    faturalar backend tamamlandı.

--- ServerApp/Controllers/FaturaController.cs
+++ ServerApp/Controllers/FaturaController.cs
@@ -12,23 +12,97 @@ namespace ServerApp.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class FaturaController : ControllerBase
+    public class FaturalarController : ControllerBase
     {
         private OtelContext _context;
-        public FaturaController(OtelContext context)
+        public FaturalarController(OtelContext context)
         {
             _context = context;
         }
 
-        // localhost:5000/api/guests
+        // localhost:5000/api/faturalar
         [HttpGet]
-        public async Task<ActionResult> GetGuests()
+        public async Task<ActionResult> GetFaturalar()
         {
             var faturalar = await _context
             .Faturalar
             .Select(p => FaturaToDTO(p))
             .ToListAsync();
             return Ok(faturalar);
+        }
+
+        // localhost:5000/api/faturalar/2
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFaturalar(int id)
+        {
+            var p = await _context.Faturalar.FindAsync(id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return Ok(FaturaToDTO(p));
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFatura(Fatura fatura1)
+        {
+            _context.Faturalar.Add(fatura1);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetFaturalar), new { id = fatura1.Id }, FaturaToDTO(fatura1));
+        }
+
+        // localhost/api/guest/3  
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateFatura(int id, Fatura fatura2)
+        {
+            if (id != fatura2.Id)
+            {
+                return BadRequest();
+            }
+
+            var fatura = await _context.Faturalar.FindAsync(id);
+            if (fatura == null)
+            {
+                return NotFound();
+            }
+
+            fatura.Unvan = fatura2.Unvan;
+            fatura.Adres = fatura2.Adres;
+            fatura.VergiDairesi = fatura2.VergiDairesi;
+            fatura.VergiNo = fatura2.VergiNo;
+            fatura.Not1 = fatura2.Not1;
+            fatura.Not2 = fatura2.Not2;
+            
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+
+                return NotFound();
+            }
+
+            return NoContent();
+
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFatura(int id)
+        {
+            var fatura = await _context.Faturalar.FindAsync(id);
+            if (fatura == null)
+            {
+                return NotFound();
+            }
+
+            _context.Faturalar.Remove(fatura);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
 
         private static FaturaDTO FaturaToDTO(Fatura g)
