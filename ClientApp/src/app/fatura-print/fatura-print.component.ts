aa7169f563b114528f5891e991921cb8dd75be97
mucahitgoktas <mucahitgoktas@gmail.com>
2021-07-31 21:13:39 +0300

    kdv hesaplama tamam. sırada rakamı yazıya çevirme var

--- ClientApp/src/app/fatura-print/fatura-print.component.ts
+++ ClientApp/src/app/fatura-print/fatura-print.component.ts
@@ -15,23 +15,43 @@ export class FaturaPrintComponent implements OnInit {
 
   @Input() fatura: Fatura;
   @Input() faturalar: Fatura[];
-  tarih : number = Date.now();
-  
+  tarih: number = Date.now();
 
-  
+
+
 
   constructor(private faturaService: FaturaService) { }
 
   ngOnInit(): void {
 
-       
+
   }
 
-  
+  fiyat: number;
+  kdv: number;
+  kdvHaric: number;
+  kdvFiyat: number;
+  sonuc: number;
+  kdvHaricVrg: string;
+  kdvFiyatVrg: string;
 
 
-    
-	
+
+
+
+  kdvHesapla() {
+    this.kdvHaric = this.fiyat / (1 + this.kdv / 100);
+    this.kdvHaricVrg = this.kdvHaric.toFixed(4);
+    this.kdvFiyat = this.fiyat - this.kdvHaric;
+    this.kdvFiyatVrg = this.kdvFiyat.toFixed(4);
+    this.sonuc = this.fiyat;
+  }
+
+
+
+
+
+
 
 
 }
